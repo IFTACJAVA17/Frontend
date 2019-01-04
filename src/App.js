1e582cf17ac9833dc889598286993c87c79ff644
Patrik Olin <enmejl@gmail.com>
2019-01-04 09:40:38 +0100

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import Navigation from './components/navbar/navigation';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
-      <Navigation />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
