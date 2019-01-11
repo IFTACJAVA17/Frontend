4aab9a3937e977ac9f28f10547f1d728d6dd9ed0
Marjo94 <Marcus_johansson94@outlook.com>
2019-01-11 13:05:32 +0100

    Added Firebase, Login bugg still exists

--- src/components/login-component/LoginButtons.jsx
+++ src/components/login-component/LoginButtons.jsx
@@ -4,14 +4,26 @@ import { Button, Row, Col } from 'reactstrap';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faIgloo } from '@fortawesome/free-solid-svg-icons'
+import Firebase from '../Firebase/firebase.js';
 
 
 library.add(faIgloo)
+
+const INITIAL_STATE = {
+    email: 'marcusjohanss94@gmail.com',
+    password: 'BLAJblaj',
+    error: null
+}
 
 class SigninButtons extends React.Component {
 
     constructor(props){
         super(props);
+    }
+
+    testLogin(){
+    Firebase.doSignIn(INITIAL_STATE.email, INITIAL_STATE.password);
+    
     }
     
     render (){
@@ -20,7 +32,7 @@ class SigninButtons extends React.Component {
                 <div className="login-button-div">
                     <Row className="signin-button-row">
                         <Col>
-                            <Button className='btn-social btn-google signin-button' /*onClick={() => props.login(googleProvider)}*/><i className="fab fa-google icon-fixer"></i>Sign in</Button>
+                            <Button className='btn-social btn-google signin-button' onClick={this.testLogin}><i className="fab fa-google icon-fixer"></i>Sign in</Button>
                         </Col>
                     </Row>
                     <Row className="signin-button-row">
