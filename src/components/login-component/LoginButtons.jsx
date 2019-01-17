302fc1b9002672e5cd1e18d324e1e171ed906647
Patrik Holmkvist <patrik.holmkvist@outlook.com>
2019-01-17 14:16:24 +0100

    IG-47 Implemented redux logic

--- src/components/login-component/LoginButtons.jsx
+++ src/components/login-component/LoginButtons.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-//import { googleProvider, facebookProvider } from '../bin/firebase.js';
 import { Button, Row, Col } from 'reactstrap';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faIgloo } from '@fortawesome/free-solid-svg-icons'
-import { signInWithEmailAndPassword, signInWithProvider, googleProvider, facebookProvider } from '../Firebase/firebase.js';
-
+import { connect } from 'react-redux';
+import { signIn } from '../../redux/actions/index';
+import { googleAuthProvider, facebookAuthProvider } from '../../config/firebase';
 
 library.add(faIgloo)
 
@@ -16,11 +16,11 @@ const INITIAL_STATE = {
 
 class SigninButtons extends React.Component {
 
-    login(provider){
-        //signInWithEmailAndPassword(INITIAL_STATE.email, INITIAL_STATE.password);
-        //signInWithProvider(googleProvider); //loggar in med google. 
-        signInWithProvider(provider);
-    }
+    // login(provider){
+    //     //signInWithEmailAndPassword(INITIAL_STATE.email, INITIAL_STATE.password);
+    //     //signInWithProvider(googleProvider); //loggar in med google. 
+    //     signInWithProvider(provider);
+    // }
     
     render (){
        return <div>
@@ -28,12 +28,12 @@ class SigninButtons extends React.Component {
                 <div className="login-button-div">
                     <Row className="signin-button-row">
                         <Col>
-                            <Button className='btn-social btn-google signin-button' onClick={() => this.login(googleProvider)}><i className="fab fa-google icon-fixer"></i>Sign in</Button>
+                            <Button className='btn-social btn-google signin-button' onClick={signIn(googleAuthProvider)}><i className="fab fa-google icon-fixer"></i>Sign in</Button>
                         </Col>
                     </Row>
                     <Row className="signin-button-row">
                         <Col>
-                            <Button className='btn-social btn-facebook signin-button' onClick={() => this.login(facebookProvider)}><i className="fab fa-facebook icon-fixer"></i>Sign in</Button>
+                            <Button className='btn-social btn-facebook signin-button' onClick={signIn(facebookAuthProvider)}><i className="fab fa-facebook icon-fixer"></i>Sign in</Button>
                         </Col>
                     </Row>
                 </div>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import firebase, { FirebaseContext } from './components/Firebase';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 
 ReactDOM.render(
-    <FirebaseContext.Provider value={firebase}>
+    <Provider store={store}>
         <App />
-    </FirebaseContext.Provider>, 
-    document.getElementById('root'));
+    </Provider>
+    , document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
