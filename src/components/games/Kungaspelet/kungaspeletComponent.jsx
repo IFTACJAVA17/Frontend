023d17c14430eb208c8273e6ed2e0400ba89bb35
Emil Sundvall <emil@sndvll.com>
2019-01-23 14:02:12 +0100

    added user to kungens alla hattar, and persitence of highscore

--- src/components/games/Kungaspelet/kungaspeletComponent.jsx
+++ src/components/games/Kungaspelet/kungaspeletComponent.jsx
@@ -1,36 +1,75 @@
 import React, { Component } from 'react';
 import Iframe from 'react-iframe';
+import { connect } from 'react-redux';
+import * as actions from '../../../redux/actions/index';
+import { highscoreRef } from '../../../config/firebase'
 
-export default class KungaspeletComponent extends Component {
- 
+class KungaspeletComponent extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            score: 0
+            score: 0,
+            scores: []
         };
     }
 
     componentDidMount() {
         window.addEventListener('message', this.addScoreToState);
+        highscoreRef.child('kungensallahattar').on('value', (snap) => this.fetchHighscore(snap));
+    }
+
+    componentWillMount(){
+        let scores = [];
+        this.fetchHighscore = (snap) => {
+            if(snap.val() !== undefined) {
+                scores = snap.val(); 
+                this.setState({scores});
+            }
+        }
+        this.addScoreToDatabase = (scoreObj) => {
+            const scores = this.state.scores;
+            scores.push(scoreObj);
+            highscoreRef.child('kungensallahattar').set(scores);
+        }
+    }
+
+
+    componentWillUnmount(){
+        highscoreRef.child('kungensallahattar').off('value', this.fetchHighscore);
     }
 
     addScoreToState = (e) => {
         if (e.origin !== 'https://patrikolin.github.io') {
             return;
         } else {
+            
             this.setState({
-                score: e.data
+                score: e.data,
+            }, () => {
+                if(e.data > 0) {
+                    const scoreObj = { score: e.data, user: this.props.user.displayName}
+                    this.addScoreToDatabase(scoreObj)
+                } else {
+                    console.log('you lost');
+                }
             })
         }
     }
 
- 
+
     render() {
         return (
-            <Iframe url= "https://patrikolin.github.io/Kungaspelet/"
-            width="92%"
-            height="85%"
+            <Iframe url="https://patrikolin.github.io/Kungaspelet/"
+                width="92%"
+                height="85%"
             />
-            );
-        }
+        );
+    }
 }
+
+const mapStateToProps = ({ user }) => {
+    return { user };
+}
+
+export default connect(mapStateToProps, actions)(KungaspeletComponent);
